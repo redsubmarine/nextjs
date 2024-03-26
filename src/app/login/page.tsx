d627eef92041c27672e22c431e97608932886581
Wonseok Yang <wsyang@digicaps.com>
2024-03-26 14:15:49 +0900

    node version fix. prettier setup

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from 'react'
+
+interface LoginPageProps {}
+
+const LoginPage: FunctionComponent<LoginPageProps> = () => {
+  return <div>LoginPage</div>
+}
+
+export default LoginPage
