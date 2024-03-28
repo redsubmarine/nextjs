a6d99010a6a98f320e1b62e082f270d7c3bc197b
Wonseok Yang <wsyang@digicaps.com>
2024-03-28 15:01:14 +0900

    apply add products page design

---
+++ src/app/home/products/add/page.tsx
@@ -0,0 +1,28 @@
+import { FunctionComponent } from 'react'
+import styles from '@/app/ui/home/products/addProduct/addProduct.module.css'
+
+interface AddProductProps {}
+
+const AddProduct: FunctionComponent<AddProductProps> = () => {
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <input type="text" placeholder="title" name="title" required />
+        <select name="cat" id="cat">
+          <option value="general">Choose a Category</option>
+          <option value="kitchen">Kitchen</option>
+          <option value="phone">Phone</option>
+          <option value="computer">Computer</option>
+        </select>
+        <input type="number" placeholder="price" name="price" />
+        <input type="number" placeholder="stock" name="stock" />
+        <input type="text" placeholder="color" name="color" />
+        <input type="text" placeholder="size" name="size" />
+        <textarea name="desc" id="desc" rows={16} placeholder="Description"></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default AddProduct
