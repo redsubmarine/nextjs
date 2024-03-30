a9ed634fe152c4c618168c6810dafebb3af2c469
red <wonseok@duck.com>
2024-03-30 18:27:21 +0900

    apply login design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,19 @@
+import styles from '@/app/ui/login/login.module.css'
 import { FunctionComponent } from 'react'
 
 interface LoginPageProps {}
 
 const LoginPage: FunctionComponent<LoginPageProps> = () => {
-  return <div>LoginPage</div>
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1>Login</h1>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  )
 }
 
 export default LoginPage
