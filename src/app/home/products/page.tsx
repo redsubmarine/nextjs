a4997096f58718ad759b8c38d4291039e54034ca
Wonseok Yang <wsyang@digicaps.com>
2024-04-02 18:32:36 +0900

    add pagination for products

--- src/app/home/products/page.tsx
+++ src/app/home/products/page.tsx
@@ -1,13 +1,23 @@
+import { fetchProducts } from '@/app/lib/data'
 import Pagination from '@/app/ui/home/pagination/pagination'
 import styles from '@/app/ui/home/products/products.module.css'
 import Search from '@/app/ui/home/search/search'
 import Image from 'next/image'
 import Link from 'next/link'
+import { ReadonlyURLSearchParams } from 'next/navigation'
 import { FunctionComponent } from 'react'
 
-interface ProductsPageProps {}
+interface ProductsPageProps {
+  searchParams?: ReadonlyURLSearchParams
+}
 
-const ProductsPage: FunctionComponent<ProductsPageProps> = () => {
+const ProductsPage: FunctionComponent<ProductsPageProps> = async ({ searchParams }) => {
+  const sp = new URLSearchParams(searchParams)
+  const q = sp.get('q') || ''
+  const page = parseInt(sp.get('page') || '1') || 1
+
+  const { products, count } = await fetchProducts(q, page)
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -28,31 +38,41 @@ const ProductsPage: FunctionComponent<ProductsPageProps> = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <div className={styles.product}>
-                <Image src="/noproduct.jpg" alt="no avatar" width={40} height={40} className={styles.productImage} />
-                iPhone
-              </div>
-            </td>
-            <td>Desc</td>
-            <td>$999</td>
-            <td>2024.03.27.</td>
-            <td>72</td>
-            <td>
-              <div className={styles.buttons}>
-                <Link href="/">
-                  <button className={`${styles.button} ${styles.view}`}>View</button>
-                </Link>
-                <Link href="/">
-                  <button className={`${styles.button} ${styles.delete}`}>Delete</button>
-                </Link>
-              </div>
-            </td>
-          </tr>
+          {products.map((product) => {
+            return (
+              <tr key={product.id}>
+                <td>
+                  <div className={styles.product}>
+                    <Image
+                      src={product.image || '/noproduct.jpg'}
+                      alt="no avatar"
+                      width={40}
+                      height={40}
+                      className={styles.productImage}
+                    />
+                    {product.title}
+                  </div>
+                </td>
+                <td>{product.description}</td>
+                <td>${product.price}</td>
+                <td>{product.createdAt?.toString().slice(4, 16)}</td>
+                <td>{product.stock}</td>
+                <td>
+                  <div className={styles.buttons}>
+                    <Link href={`/home/products/${product.id}`}>
+                      <button className={`${styles.button} ${styles.view}`}>View</button>
+                    </Link>
+                    <Link href="/">
+                      <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
-      <Pagination />
+      <Pagination count={count} />
     </div>
   )
 }
--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,4 +1,4 @@
-import { User } from './models'
+import { Product, User } from './models'
 import { connectToDB } from './utils'
 
 export const fetchUsers = async (q: string, page: number) => {
@@ -18,5 +18,25 @@ export const fetchUsers = async (q: string, page: number) => {
   } catch (err) {
     console.log(err)
     throw new Error('Failed to fetch users!')
+  }
+}
+
+export const fetchProducts = async (q: string, page: number) => {
+  const $regex = new RegExp(q, 'i')
+
+  const ITEM_PER_PAGE = 2
+
+  try {
+    await connectToDB()
+    const count = await Product.find({ title: { $regex } }).countDocuments()
+
+    const products = await Product.find({ title: { $regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1))
+
+    return { products, count }
+  } catch (err) {
+    console.log(err)
+    throw new Error('Failed to fetch products!')
   }
 }
